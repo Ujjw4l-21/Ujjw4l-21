458074602dbdde23ef809f3900fb8373192246cd
Ujjw4l-21 <sunilnisha064@gmail.com>
2026-04-08 11:50:33 +0530

    Delete StartPortal.bat

--- StartPortal.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Akshaya Portal Server
-echo Starting Database and Portal...
-cd /d "%~dp0"
-:: Starts the server for the whole network
-start /b npx json-server --watch db.json --host 0.0.0.0 --port 3000
-:: Opens the portal locally
-start "" "index.html"
-exit
